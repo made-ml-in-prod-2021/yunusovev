776c97093f243d5d98d6980615da2e0ceaf80ebd
YunusovEgor <evyunusov@domclick.ru>
2021-05-09 22:42:30 +0300

    update setup.py

--- ml_project/setup.py
+++ ml_project/setup.py
@@ -16,16 +16,16 @@ setup(
         'marshmallow>=3.11.1',
         'marshmallow-dataclass>=8.4.1',
         'click>=7.1.2,<7.3',
-        'Faker>=8.1.2,<8.2',
-        'flake8>=3.9.1'
     ],
     extras_require={
         'test': [
             'pytest>=6.2.4,<6.3',
             'pytest-cov>=2.11.1,<3.0.0',
+            'Faker>=8.1.2,<8.2',
         ],
         'lint': [
             'mypy>=0.812,<1.0',
+            'flake8>=3.9.1'
         ],
     },
 )
